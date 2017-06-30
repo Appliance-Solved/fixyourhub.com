35fb23916427c45ed611fcb116246a24a2c06436
larryg727 <larrygonzales727@gmail.com>
2017-06-30 17:24:25 -0500

    initial commit

---
+++ src/main/java/com/codeup/FixyourhubApplication.java
@@ -0,0 +1,12 @@
+package com.codeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixyourhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixyourhubApplication.class, args);
+	}
+}
