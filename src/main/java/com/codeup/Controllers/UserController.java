5c591b0742f34d830df9fe92017fbcb0b0f8b909
larryg727 <larrygonzales727@gmail.com>
2017-07-05 14:49:53 -0500

    added controller and get mapping for index page

---
+++ src/main/java/com/codeup/Controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.codeup.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by larryg on 7/5/17.
+ */
+@Controller
+public class UserController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+
+    }
+}
