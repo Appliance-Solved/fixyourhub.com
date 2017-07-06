4b61dfc96a4c49d40f5dc375f5e2ad4bd962f5b3
larryg727 <larrygonzales727@gmail.com>
2017-07-06 14:11:22 -0500

    implemented spring security configurations

--- src/main/java/com/codeup/Repositories/UserRolesRepository.java
+++ src/main/java/com/codeup/Repositories/UserRolesRepository.java
@@ -1,11 +1,16 @@
 package com.codeup.Repositories;
 
 import com.codeup.Models.UserRoles;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by Carlos on 7/5/17.
  */
 public interface UserRolesRepository extends CrudRepository<UserRoles, Long> {
+    @Query("select ur.role from UserRole ur, User u where u.username=?1 and ur.userId = u.id")
+    public List<String> ofUserWith(String username);
 
 }
---
+++ src/main/java/com/codeup/Services/SecurityConfiguration.java
@@ -0,0 +1,57 @@
+package com.codeup.Services;
+
+import com.codeup.Models.UserWithRoles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Created by larryg on 7/6/17.
+ */
+    @Configuration
+    @EnableWebSecurity
+    @ComponentScan(basePackageClasses = UserWithRoles.class)
+    public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        private UserDetailsLoader userDetails;
+
+        @Bean(name = "passwordEncoder")
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/") // user's home page, it can be any URL
+                    .permitAll() // Anyone can go to the login page
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/logout") // anyone can see the home and logout page
+                    .permitAll()
+                    .and()
+                    .logout()
+                    .logoutSuccessUrl("/login?logout") // append a query string value
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/dashboard") // only authenticated users can create ads
+                    .authenticated()
+            ;
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetails).passwordEncoder(passwordEncoder());
+        }
+    }
+
