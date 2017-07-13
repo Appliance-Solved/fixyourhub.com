0547f0836683505992bb90807f4e03f25be61546
Carlos <c.soto93@hotmail.com>
2017-07-12 21:03:13 -0500

    added page for user to write complaint and pass it to the view servicer page.

--- src/main/java/com/codeup/Controllers/UserController.java
+++ src/main/java/com/codeup/Controllers/UserController.java
@@ -138,7 +138,7 @@ public class UserController {
     @GetMapping("/user/scheduleservice")
     public String scheduleService(Model model) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        model.addAttribute("user", user);
+//        model.addAttribute("user", user);
         Iterable<UserAppliance> userAppliances = userAppliancesSvc.findAllByUser(user);
         model.addAttribute("userAppliances", userAppliances);
         UserAppliance userAppliance = new UserAppliance();
@@ -146,18 +146,30 @@ public class UserController {
         return "user/schedule-service";
     }
 
+    @GetMapping("/user/scheduleservice/complaint")
+    public String scheduleServiceComplaint(
+            @RequestParam(name = "applianceId") long applianceId,
+            Model model
+    ){
+        model.addAttribute("applianceId", applianceId);
+        return "user/schedule-service-complaint";
+    }
+
     @GetMapping("/user/scheduleservice/day")
     public String scheduleServiceDate(
             @RequestParam(name = "applianceId") long applianceId,
+            @RequestParam(name = "complaint") String complaint,
             Model model
-    ) {
+    ){
         model.addAttribute("applianceId", applianceId);
+        model.addAttribute("complaint", complaint);
         return "user/schedule-service-date";
     }
 
     @GetMapping("/user/scheduleservice/results")
     public String serviceSearchResults(
             @RequestParam(name = "applianceId") long applianceId,
+            @RequestParam(name = "complaint") String complaint,
             @RequestParam(name = "time-frame") int timeFrame,
             Model model
     ) {
@@ -173,19 +185,25 @@ public class UserController {
                 servicers.add(user);
             }
         }
+        model.addAttribute("complaint", complaint);
         model.addAttribute("servicers", servicers);
         return "user/servicers-results";
 
     }
 
     @GetMapping("/user/viewservicer")
-    public String showServicerProfile(@RequestParam(name = "id") long id, Model model) {
+    public String showServicerProfile(
+            @RequestParam(name = "id") long id,
+            @RequestParam(name = "complaint") String complaint,
+            Model model
+    ) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("user", user);
         User servicer = userSvc.findOne(id);
         model.addAttribute("servicer", servicer);
         Servicer servicer_info = servicerSvc.findServicerInfoByUserId(servicer);
         model.addAttribute("servicer_info", servicer_info);
+        model.addAttribute("complaint", complaint);
         return "user/viewservicer";
     }
 
