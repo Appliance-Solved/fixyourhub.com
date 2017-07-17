2609829b5f0509e77c1334d04475a27235b9a681
larryg727 <larrygonzales727@gmail.com>
2017-07-16 22:28:18 -0500

    completed email to be sent out after a technician is  assigned and appointment is confirmed

--- src/main/java/com/codeup/Controllers/ServicerController.java
+++ src/main/java/com/codeup/Controllers/ServicerController.java
@@ -285,10 +285,11 @@ public class ServicerController {
         @PostMapping("/servicer/appointment/confirm")
     public String confirmAppointment(@RequestParam(name = "confirm_id")Long id, @RequestParam(name = "tech_id")Long tech_id){
         Technician tech = techsvc.findOneById(tech_id);
-            System.out.println(tech.getName());
         Appointment appointment = appointmentSvc.findById(id);
+        Mailer mailer = new Mailer();
         appointment.setAvailable(false);
         appointmentSvc.save(appointment);
+        Mailer.send(mailer.getFrom(), mailer.getPassword(), appointment.getUser().getEmail(), mailer.getConfirmSub(), mailer.confirmMsg(appointment, tech));
         return "redirect:/servicer/dashboard";
         }
 
--- src/main/java/com/codeup/Services/Mailer.java
+++ src/main/java/com/codeup/Services/Mailer.java
@@ -1,5 +1,6 @@
 package com.codeup.Services;
 import com.codeup.Models.Appointment;
+import com.codeup.Models.Technician;
 
 import javax.mail.*;
 import javax.mail.internet.*;
@@ -100,18 +101,35 @@ public class Mailer {
     }
 
   public String requestMsg(Appointment appointment){
-      String message = "<h1>Fixithub.solutions</h1>";
-      message += "<h2>You have received a new service request";
+      String message = "<h1 style='text-align: center'>Fixithub.solutions</h1>";
+      message += "<h2 style='text-align: center'>You have received a new service request</h2>";
       message += "<p>" + appointment.getUser().getName() +  " has requested your service on their appliance.</p>" +
               "<p>Below are the details of the request:</p>" +
               "<p>Requested for " + appointment.formatDate(appointment.getDate()) + " between " + appointment.formatTime(appointment.getStartTime()) + "-" + appointment.formatTime(appointment.getStopTime()) +  "</p>" +
-              "<p>Address: " + appointment.getUser().getAddress() + "</p>" +
-              "<p>" + appointment.getUser().getCity() + ", " + appointment.getUser().getState() + " " + appointment.getUser().getZipcode() + "</p>" +
+              "<p>Address: " + appointment.getUser().getAddress() + "<br />" +
+              appointment.getUser().getCity() + ", " + appointment.getUser().getState() + " " + appointment.getUser().getZipcode() + "</p>" +
               "<p>Complaint: " + printApplianceType(appointment.getServiceRecords().getUserAppliance().getAppliance_id()) + "- " + appointment.getServiceRecords().getComplaint() + "</p>" +
-              "<br /><p>Please <a href='fixithub.solutions/login'>login</a> to confirm or decline this appointment</p>" +
-              "<br /><br /><p>-Thank You,<br /> From the Fixithub Team</p>";
+              "<br /><p>Please <a href='http://fixithub.solutions/login'>login</a> to confirm or decline this appointment</p>" +
+              "<br /><br /><p>-Thank You,<br />The Fixithub Team</p>";
 
     return message;
+  }
+
+  public String confirmMsg (Appointment appointment, Technician technician) {
+      String message = "<h1 style='text-align: center'>Thank you for using Fixithub</h1>" +
+              "<h2 style='text-align: center'>Your service appointment has been confirmed by " + appointment.getServicer().getName() + "</h2>" +
+              "<p>Your service request information is as follows:</p>" +
+              "<p> Scheduled for: " + appointment.formatDate(appointment.getDate()) + " between " + appointment.formatTime(appointment.getStartTime()) + "-" + appointment.formatTime(appointment.getStopTime()) +  "</p>" +
+              "<p>Service complaint: " + printApplianceType(appointment.getServiceRecords().getUserAppliance().getAppliance_id()) + "- " + appointment.getServiceRecords().getComplaint() + "</p>" +
+              "<p>Your address: " + appointment.getUser().getAddress() + "<br />" +
+               appointment.getUser().getCity() + ", " + appointment.getUser().getState() + " " + appointment.getUser().getZipcode() + "</p>" +
+                "<br />" +
+              "Thank you for choosing " + appointment.getServicer().getName() + " for your appliance repair needs. If you have any questions or anything on this service request does not look correct, please contact us at " +
+              appointment.getServicer().getPhone() + " or by email at " + appointment.getServicer().getEmail() + ".</p>" +
+              "<br /> <p>The technician assigned to your appointment will be " + technician.getName() + ". For your convenience and safety we have included a picture of them.</p>" +
+              "<div><img src='http://fixithub.solutions/uploads/" + technician.getPicUrl() + "' style='max-width: 150px' /></div>" +
+              "<br /><br /><p>-Thank You,<br />The Fixithub Team</p>";
+              return message;
   }
 }
 
